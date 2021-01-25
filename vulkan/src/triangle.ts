d909928710aea6ed5b8b662d9153970aa21b3b89
vinneyto <vinneyto@gmail.com>
2021-01-25 10:34:38 +0300

    adds presence queue extraction

--- vulkan/src/triangle.ts
+++ vulkan/src/triangle.ts
@@ -26,29 +26,177 @@ import {
   VkDeviceCreateInfo,
   VkDevice,
   vkCreateDevice,
+  VkQueue,
+  vkGetDeviceQueue,
+  VkSurfaceKHR,
+  vkGetPhysicalDeviceSurfaceSupportKHR,
 } from "nvk/generated/1.1.126/win32";
 
 const VALIDATION_LAYERS = ["VK_LAYER_KHRONOS_validation"];
 
-(function () {
-  const win = new VulkanWindow({
-    width: 800,
-    height: 600,
-    title: "nvk triangle",
-  });
+class QueueFamilyIndices {
+  graphicsFamily?: number;
+  presentFamily?: number;
 
-  const validationLayers = getCurrentValidationLayers();
-  const instance = createInstance(validationLayers, win);
-  const physicaDevice = pickPhysicalDevice(instance);
-  const device = createLogicalDevice(physicaDevice, validationLayers);
+  isComplete() {
+    return (
+      this.graphicsFamily !== undefined && this.presentFamily !== undefined
+    );
+  }
+}
 
-  console.log("drawing..");
-  (function drawLoop() {
-    if (!win.shouldClose()) setTimeout(drawLoop, 0);
-    // drawFrame();
-    win.pollEvents();
-  })();
-})();
+class Renderer {
+  win!: VulkanWindow;
+  instance!: VkInstance;
+  physicalDevice!: VkPhysicalDevice;
+  device!: VkDevice;
+  validationLayers!: string[];
+  graphicsQueue!: VkQueue;
+  presentQueue!: VkQueue;
+  surface!: VkSurfaceKHR;
+
+  constructor() {
+    this.win = new VulkanWindow({
+      width: 800,
+      height: 600,
+      title: "nvk triangle",
+    });
+
+    this.initValidationLayers();
+    this.initInstance();
+    this.initSurface();
+    this.initPhysicalDevice();
+    this.initLogicalDevice();
+  }
+
+  initValidationLayers() {
+    const layerCount = { $: 0 };
+    vkEnumerateInstanceLayerProperties(layerCount, null);
+
+    const availableLayers = makeArray(VkLayerProperties, layerCount.$);
+    vkEnumerateInstanceLayerProperties(layerCount, availableLayers);
+
+    this.validationLayers = VALIDATION_LAYERS.filter((l) =>
+      availableLayers.some((al) => al.layerName === l)
+    );
+  }
+
+  initInstance() {
+    const appInfo = createAppInfo();
+    const createInfo = new VkInstanceCreateInfo();
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = appInfo;
+
+    const instanceExtensions = this.win.getRequiredInstanceExtensions();
+    createInfo.enabledExtensionCount = instanceExtensions.length;
+    createInfo.ppEnabledExtensionNames = instanceExtensions;
+
+    if (this.validationLayers.length > 0) {
+      createInfo.enabledLayerCount = this.validationLayers.length;
+      createInfo.ppEnabledLayerNames = this.validationLayers;
+    } else {
+      createInfo.enabledLayerCount = 0;
+    }
+
+    console.log("extensions", createInfo.ppEnabledExtensionNames);
+    console.log("layers", createInfo.ppEnabledLayerNames);
+
+    const instance = new VkInstance();
+
+    let result: VkResult;
+
+    result = vkCreateInstance(createInfo, null, instance);
+    ASSERT_VK_RESULT(result);
+
+    this.instance = instance;
+  }
+
+  initSurface() {
+    this.surface = new VkSurfaceKHR();
+
+    this.win.createSurface(this.instance, null, this.surface);
+  }
+
+  initPhysicalDevice() {
+    const deviceCount = { $: 0 };
+    vkEnumeratePhysicalDevices(this.instance, deviceCount, null);
+
+    if (deviceCount.$ === 0) {
+      throw new Error("no devices with Vulkan support");
+    }
+
+    const devices = makeArray(VkPhysicalDevice, deviceCount.$);
+    vkEnumeratePhysicalDevices(this.instance, deviceCount, devices);
+
+    let raitings = devices
+      .map((d) => rateDeviceSuitability(d, this.surface))
+      .map((raiting, index) => ({ raiting, index }))
+      .sort((a, b) => b.raiting - a.raiting);
+
+    console.log(raitings);
+
+    if (raitings.length === 0 || raitings[0].raiting === 0) {
+      throw new Error("failed to find a suitable GPU!");
+    }
+
+    this.physicalDevice = devices[raitings[0].index];
+  }
+
+  initLogicalDevice() {
+    const indices = findQueueFamilies(this.physicalDevice, this.surface);
+
+    const queueCreateInfos: VkDeviceQueueCreateInfo[] = [];
+    const uniqueQueueFamilies = [
+      indices.graphicsFamily!,
+      indices.presentFamily!,
+    ];
+
+    for (const queueFamily of uniqueQueueFamilies) {
+      const queueCreateInfo = new VkDeviceQueueCreateInfo();
+      queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+      queueCreateInfo.queueFamilyIndex = queueFamily;
+      queueCreateInfo.queueCount = 1;
+      queueCreateInfo.pQueuePriorities = new Float32Array([1]);
+      queueCreateInfos.push(queueCreateInfo);
+    }
+
+    const deviceFeatures = new VkPhysicalDeviceFeatures();
+    const deviceCreateInfo = new VkDeviceCreateInfo();
+
+    deviceCreateInfo.pQueueCreateInfos = queueCreateInfos;
+    deviceCreateInfo.queueCreateInfoCount = queueCreateInfos.length;
+
+    deviceCreateInfo.pEnabledFeatures = deviceFeatures;
+    deviceCreateInfo.enabledExtensionCount = 0;
+
+    if (this.validationLayers.length > 0) {
+      deviceCreateInfo.enabledLayerCount = this.validationLayers.length;
+      deviceCreateInfo.ppEnabledLayerNames = this.validationLayers;
+    } else {
+      deviceCreateInfo.enabledLayerCount = 0;
+    }
+
+    const device = new VkDevice();
+
+    const result = vkCreateDevice(
+      this.physicalDevice,
+      deviceCreateInfo,
+      null,
+      device
+    );
+    ASSERT_VK_RESULT(result);
+
+    this.device = device;
+
+    //
+    this.graphicsQueue = new VkQueue();
+    vkGetDeviceQueue(device, indices.graphicsFamily!, 0, this.graphicsQueue);
+
+    //
+    this.presentQueue = new VkQueue();
+    vkGetDeviceQueue(device, indices.presentFamily!, 0, this.presentQueue);
+  }
+}
 
 function createAppInfo() {
   const appInfo = new VkApplicationInfo();
@@ -61,68 +209,11 @@ function createAppInfo() {
   return appInfo;
 }
 
-function createInstance(validationLayers: string[], win: VulkanWindow) {
-  const appInfo = createAppInfo();
-  const createInfo = new VkInstanceCreateInfo();
-  createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-  createInfo.pApplicationInfo = appInfo;
-
-  const instanceExtensions = win.getRequiredInstanceExtensions();
-  createInfo.enabledExtensionCount = instanceExtensions.length;
-  createInfo.ppEnabledExtensionNames = instanceExtensions;
-
-  if (validationLayers.length > 0) {
-    createInfo.enabledLayerCount = validationLayers.length;
-    createInfo.ppEnabledLayerNames = validationLayers;
-  } else {
-    createInfo.enabledLayerCount = 0;
-  }
-
-  console.log("extensions", createInfo.ppEnabledExtensionNames);
-  console.log("layers", createInfo.ppEnabledLayerNames);
-
-  const instance = new VkInstance();
-
-  let result: VkResult;
-
-  result = vkCreateInstance(createInfo, null, instance);
-  ASSERT_VK_RESULT(result);
-
-  return instance;
-}
-
 function makeArray<T>(Ctor: new () => T, count: number) {
   return [...Array(count)].map(() => new Ctor());
 }
 
-function pickPhysicalDevice(instance: VkInstance) {
-  const deviceCount = { $: 0 };
-  vkEnumeratePhysicalDevices(instance, deviceCount, null);
-
-  if (deviceCount.$ === 0) {
-    throw new Error("no devices with Vulkan support");
-  }
-
-  const devices = makeArray(VkPhysicalDevice, deviceCount.$);
-  vkEnumeratePhysicalDevices(instance, deviceCount, devices);
-
-  let raitings = devices
-    .map(rateDeviceSuitability)
-    .map((raiting, index) => ({ raiting, index }))
-    .sort((a, b) => b.raiting - a.raiting);
-
-  if (raitings.length === 0 || raitings[0].raiting === 0) {
-    throw new Error("failed to find a suitable GPU!");
-  }
-
-  return devices[raitings[0].index];
-}
-
-interface QueueFamilyIndices {
-  graphicsFamily?: number;
-}
-
-function findQueueFamilies(device: VkPhysicalDevice) {
+function findQueueFamilies(device: VkPhysicalDevice, surface: VkSurfaceKHR) {
   const queueFamilyCount = { $: 0 };
   vkGetPhysicalDeviceQueueFamilyProperties(device, queueFamilyCount, null);
 
@@ -133,12 +224,19 @@ function findQueueFamilies(device: VkPhysicalDevice) {
     queueFamilies
   );
 
-  const indices: QueueFamilyIndices = {};
+  const indices = new QueueFamilyIndices();
 
   let i = 0;
   for (const queueFamily of queueFamilies) {
     if (queueFamily.queueFlags & VkQueueFlagBits.VK_QUEUE_GRAPHICS_BIT) {
       indices.graphicsFamily = i;
+    }
+
+    const presentSupport = { $: false };
+    vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface, presentSupport);
+
+    if (presentSupport.$) {
+      indices.presentFamily = i;
     }
 
     i++;
@@ -147,14 +245,17 @@ function findQueueFamilies(device: VkPhysicalDevice) {
   return indices;
 }
 
-function rateDeviceSuitability(device: VkPhysicalDevice) {
+function rateDeviceSuitability(
+  device: VkPhysicalDevice,
+  surface: VkSurfaceKHR
+) {
   const properties = new VkPhysicalDeviceProperties();
   const features = new VkPhysicalDeviceFeatures();
 
   vkGetPhysicalDeviceProperties(device, properties);
   vkGetPhysicalDeviceFeatures(device, features);
 
-  const queueIndices = findQueueFamilies(device);
+  const queueIndices = findQueueFamilies(device, surface);
 
   console.log({
     deviceName: properties.deviceName,
@@ -168,7 +269,7 @@ function rateDeviceSuitability(device: VkPhysicalDevice) {
 
   let score = 0;
 
-  if (queueIndices.graphicsFamily === undefined) {
+  if (!queueIndices.isComplete()) {
     return 0;
   }
 
@@ -188,60 +289,20 @@ function rateDeviceSuitability(device: VkPhysicalDevice) {
   return score;
 }
 
-function createLogicalDevice(
-  physicalDevice: VkPhysicalDevice,
-  layers: string[]
-) {
-  const indices = findQueueFamilies(physicalDevice);
-
-  const queueCreateInfo = new VkDeviceQueueCreateInfo();
-  queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-  queueCreateInfo.queueFamilyIndex = indices.graphicsFamily as number;
-  queueCreateInfo.queueCount = 1;
-  queueCreateInfo.pQueuePriorities = new Float32Array([1]);
-
-  const deviceFeatures = new VkPhysicalDeviceFeatures();
-  const deviceCreateInfo = new VkDeviceCreateInfo();
-
-  deviceCreateInfo.pQueueCreateInfos = [queueCreateInfo];
-  deviceCreateInfo.queueCreateInfoCount = 1;
-
-  deviceCreateInfo.pEnabledFeatures = deviceFeatures;
-  deviceCreateInfo.enabledExtensionCount = 0;
-
-  if (layers.length > 0) {
-    deviceCreateInfo.enabledLayerCount = layers.length;
-    deviceCreateInfo.ppEnabledLayerNames = layers;
-  } else {
-    deviceCreateInfo.enabledLayerCount = 0;
-  }
-
-  const device = new VkDevice();
-
-  const result = vkCreateDevice(physicalDevice, deviceCreateInfo, null, device);
-  ASSERT_VK_RESULT(result);
-
-  return device;
-}
-
 function ASSERT_VK_RESULT(result: VkResult) {
   if (result !== VkResult.VK_SUCCESS)
     throw new Error(`Vulkan assertion failed!`);
 }
 
-function getCurrentValidationLayers() {
-  const availableValidationLayers = getAvailableValidationLayers();
-  return VALIDATION_LAYERS.filter((l) =>
-    availableValidationLayers.some((al) => al.layerName === l)
-  );
-}
+(function () {
+  const renderer = new Renderer();
 
-function getAvailableValidationLayers() {
-  const layerCount = { $: 0 };
-  vkEnumerateInstanceLayerProperties(layerCount, null);
+  console.log("Renderer consist of = ", Object.keys(renderer));
 
-  const availableLayers = makeArray(VkLayerProperties, layerCount.$);
-  vkEnumerateInstanceLayerProperties(layerCount, availableLayers);
-
-  return availableLayers;
-}
+  console.log("drawing..");
+  (function drawLoop() {
+    if (!renderer.win.shouldClose()) setTimeout(drawLoop, 0);
+    // drawFrame();
+    renderer.win.pollEvents();
+  })();
+})();
