f507a5f216b95fc39520390d3308210c1a25f950
lvinnichenko <lvinnichenko@aligntech.com>
2021-02-09 17:12:50 +0300

    adds sphere intersection check function

---
+++ RayTracing/common/color.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <vector>
+
+#include "vec3.h"
+
+void write_color(std::vector<char> &pixels, color pixel_color, int i) {
+    pixels[i + 0] = static_cast<int>(255.999 * pixel_color.x());
+    pixels[i + 1] = static_cast<int>(255.999 * pixel_color.y());
+    pixels[i + 2] = static_cast<int>(255.999 * pixel_color.z());
+    pixels[i + 3] = 255;
+}
